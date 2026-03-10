73598ae4a89a07cd343a80f29dbc05952edb0cdb
Crhistopher Gutiérrez <crhistopherisai@gmail.com>
2026-03-09 23:32:59 -0600

    home screen added and modified

---
+++ components/Home.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+export default function Home({ navigation }) {
+
+  const [nombre, setNombre] = useState('');
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Escribe tu nombre</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Tu nombre"
+        value={nombre}
+        onChangeText={setNombre}
+      />
+
+      <Button
+        title="Ir a la segunda pantalla"
+        onPress={() => navigation.navigate('About', { nombre })}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', padding: 20 },
+  titulo: { fontSize: 24, marginBottom: 20 },
+  input: {
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 20
+  }
+});
